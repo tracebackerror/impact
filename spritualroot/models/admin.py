1360aa709dbe9df2f5089cc10a1364a2e890a44a
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2022-05-04 19:27:30 +0530

    updated datetimefield

--- spritualroot/models/admin.py
+++ spritualroot/models/admin.py
@@ -4,4 +4,4 @@ from . models import ContactForm
 # Register your models here.
 @admin.register(ContactForm)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id','name','email','phone','comments','date')
+    list_display = ('id','name','email','phone','comments','created_date','modified_date')
