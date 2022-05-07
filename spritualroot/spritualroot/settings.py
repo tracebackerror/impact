f157e6d45daf747937733bca8635fbab2b1eb5bf
Shivam Chaurasia <SHIVAM9172@GMAIL.COM>
2022-05-07 23:52:43 +0530

    DRF Init

--- spritualroot/spritualroot/settings.py
+++ spritualroot/spritualroot/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'models',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- spritualroot/spritualroot/urls.py
+++ spritualroot/spritualroot/urls.py
@@ -19,4 +19,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('api-auth/', include('rest_framework.urls')),
 ]
