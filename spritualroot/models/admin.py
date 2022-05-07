4d064abcb5d7b887e8b3e509db9a3f599b5940e9
Shivam Chaurasia <SHIVAM9172@GMAIL.COM>
2022-05-07 22:47:16 +0530

    Models Commit

--- spritualroot/models/admin.py
+++ spritualroot/models/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from . models import ContactForm
+from .models import *
 
 # Register your models here.
 @admin.register(ContactForm)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id','name','email','phone','comments','created_date','modified_date')
+    list_display = ('id',
+    'first_name', 'last_name', 'email','phone','comments','created_date','modified_date')
+
+
+admin.site.register(ProjectsModel)
+admin.site.register(StoriesModel)
+admin.site.register(GetHelpModel)
+admin.site.register(VolunteerMoodel)
