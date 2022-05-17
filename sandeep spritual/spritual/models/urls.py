99bc45f55dfe208c6c03c2a3b038a0f996385a39
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2022-05-17 18:34:48 +0530

    urls file edited

--- sandeep spritual/spritual/models/urls.py
+++ sandeep spritual/spritual/models/urls.py
@@ -21,21 +21,4 @@ urlpatterns = [
     path('stories/', views.storiesview.as_view(),name='stories'),
     path('volunteers/', views.volunteersview.as_view(),name='volunteers'),
     
-
-    # path('about/',views.about,name='about'),
-    # path('blog_details/',views.blog_details,name='blog_details'),
-    # path('blog/',views.blog,name='blog'),
-    # path('contact/',views.contact,name='contact'),
-    # path('donation_details/',views.donation_details,name='donation_details'),
-    # path('donation_listing/',views.donation_listing,name='donation_listing'),
-    # path('event_details/',views.event_details,name='event_details'),
-    # path('events/',views.events,name='events'),
-    # path('faq/',views.faq,name='faq'),
-    # path('product_details/',views.product_details,name='product_details'),
-    # path('products/',views.products,name='products'),
-    # path('service/',views.service,name='service'),
-    # path('stories_details/',views.stories_details,name='stories_details'),
-    # path('stories/',views.stories,name='stories'),
-    # path('volunteers/',views.volunteers,name='volunteers'),
-    
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
